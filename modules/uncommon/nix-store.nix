4dfdd6e0c1c928f016167ae6e363098a4c0c2775
Lenni Hein <git@lennihein.com>
2025-06-04 10:14:37 +0200

    Comment description lines in nix-store module

--- modules/uncommon/nix-store.nix
+++ modules/uncommon/nix-store.nix
@@ -3,9 +3,9 @@
   # Dedupe the Nix store
   nix.settings.auto-optimise-store = true;
 
-  Garbage collection
+  # Garbage collection
   nix.gc.automatic = true;
   nix.gc.dates = "*-*-* 4:00:00";
-  without options it will only clean the store, not delete old generations
+  # without options it will only clean the store, not delete old generations
   nix.gc.options = "--delete-older-than 14d";
 }
